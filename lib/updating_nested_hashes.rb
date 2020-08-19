5837a8a035d016b6cdaaafc0310c914dc0bde76e
Sanny Do <sanny.do94@gmail.com>
2020-08-19 05:40:27 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -24,7 +24,7 @@ def update_status
   # Code your solution here:
 
 epic_tragedy[:montague][:hero][:status] = "dead"
-puts epic_tragedy
+
 
 epic_tragedy[:Capulet][:hero][:status] = "dead"
 puts epic_tragedy
