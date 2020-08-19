47e34781088bf135f3076dbce51cfcab5bf918dc
Sanny Do <sanny.do94@gmail.com>
2020-08-19 05:37:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -62,6 +62,7 @@ def add_characters
 update_status[:montague][:hero][:status] = "dead"
 
 update_status[:Capulet][:heroine][:status] = "dead"
+
 epic_tragedy[:additional_characters]= 
   prince = {name: "Prince Escalus"}
   apothecary = {name: "Apothecary"}
