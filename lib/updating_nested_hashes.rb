0cd1937b92013aebcac0b4cbaf36fbbb427fec4b
Sanny Do <sanny.do94@gmail.com>
2020-08-19 05:31:44 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -60,7 +60,7 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 epic_tragedy[:montague][:hero]
-[:status] "ead"
+[:status] dead
 
 epic_tragedy[:capulet][:hero][:status] dead
 
